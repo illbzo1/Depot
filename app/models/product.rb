db66bdeb8a281922b0c7cb79a237e94269b5d4c8
Tyler Moore <tylerlmoore@gmail.com>
2011-06-22 19:43:06 -0400

    More validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-  validates :title, :uniqueness => true
+  validates_presence_of :title, :description, :image_url, :message => 'can not be blank.'
+  validates_numericality_of :price, :greater_than_or_equal_to => 0.01, :message => 'must be a number.'
+  validates_uniqueness_of :title, :message => 'must be unique.'
   validates :image_url, :format => {
     :with     => %r{\.(gif|jpg|png)$}i,
     :message  =>  'must be a URL for GIF, JPG or PNG image.'
   }
+  validates_length_of :title, :minimum => 10, :message => 'must be at least 10 characters.'
 end
