2503c4f3ab91d0448722433a8796385def81b374
Tyler Moore <tylerlmoore@gmail.com>
2011-06-27 18:40:28 -0400

    Cart update

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @count = increment_count
+  end
+  
+  def increment_count
+    session[:counter] = 0 if session[:counter].nil?
+    session[:counter] += 1
+    @counter = session[:counter] if session[:counter] > 5
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates_presence_of :title, :description, :image_url, :message => 'can not be blank.'
   validates_numericality_of :price, :greater_than_or_equal_to => 0.01, :message => 'must be a number.'
   validates_uniqueness_of :title, :message => 'must be unique.'
@@ -8,4 +10,16 @@ class Product < ActiveRecord::Base
     :message  =>  'must be a URL for GIF, JPG or PNG image.'
   }
   validates_length_of :title, :minimum => 10, :message => 'must be at least 10 characters.'
+  
+  private
+    
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
