fed4cb81302f526d42e894dc27c0ee1d6a16abf8
Tyler Moore <tylerlmoore@gmail.com>
2011-07-25 18:49:06 -0400

    Heroku work continues!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110719214819) do
+ActiveRecord::Schema.define(:version => 20110714220725) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -41,10 +41,9 @@ ActiveRecord::Schema.define(:version => 20110719214819) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price",       :precision => 8, :scale => 2
+    t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "locale"
   end
 
   create_table "users", :force => true do |t|
